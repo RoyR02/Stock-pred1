17b82db42569ad7d70cea10a9e270faa972d723c
RoyR02 <104689216+RoyR02@users.noreply.github.com>
2024-08-10 23:51:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,45 +1,27 @@
 import streamlit as st
-import pandas as pd
-# import matplotlib.pyplot as plt
+from datetime import date
 
-# Set the title of the Streamlit app
-st.title('Stock Market Line Graph')
+import yfinance as yf
+# from prophet import Prophet
+# from prophet.plot import plot_plotly
+from plotly import graph_objs as go
 
-# Upload the CSV file
-uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+START = "2010-01-01"
+TODAY = date.today().strftime("%Y-%m-%d")
 
-if uploaded_file is not None:
-    # Read the CSV file into a DataFrame
-    df = pd.read_csv(uploaded_file)
+st.title("Stock Pred App")
 
-    # Display the DataFrame
-    st.write("Data Preview:")
-    st.dataframe(df)
+stocks = ("AAPL", "GOOG", "MSFT")
+selected_stocks = st.selectbox("Select dataset",stocks)
 
-    # Check if required columns are present in the DataFrame
-    if 'Date' in df.columns and 'Close' in df.columns:
-        # Convert the 'Date' column to datetime format
-        df['Date'] = pd.to_datetime(df['Date'])
+n_years = st.slider("Years of pred: ",1, 5)
+period = n_years * 365
 
-        # Set 'Date' as the index of the DataFrame
-        df.set_index('Date', inplace=True)
+def load_data(ticker):
+    data = yf.download(ticker, START, TODAY)
+    date.reset_index(inplace=True)
+    return data
 
-        # Plot the line graph
-        st.write("Line Graph:")
-        st.line_chart(df['Close'])
-
-        # Optionally, plot using matplotlib for more customization
-        st.write("Matplotlib Line Graph:")
-        fig, ax = plt.subplots()
-        ax.plot(df.index, df['Close'], label='Close Price')
-        ax.set_xlabel('Date')
-        ax.set_ylabel('Close Price')
-        ax.set_title('Stock Market Close Price Over Time')
-        ax.legend()
-
-        st.pyplot(fig)
-    else:
-        st.write("The CSV file does not contain the required 'Date' and 'Close' columns.")
-
-else:
-    st.write("Please upload a CSV file.")
+data_load_state = st.text("Loading......")
+data = load_data(stocks)
+data_load_state.text("Loading Data....done")
