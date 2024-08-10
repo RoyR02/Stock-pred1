db89752bdea03ed7e5e197dac70a3628166cbaeb
RoyR02 <104689216+RoyR02@users.noreply.github.com>
2024-08-10 20:02:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,8 @@ import streamlit as st
 st.title('Stock Market Prediction')
 
 st.info('Stock Market Prediction using NN')
+
+with st.expander('Data'):
+  st.write('**Raw Data**)
+  df = pd
+df = pd.read_csv
