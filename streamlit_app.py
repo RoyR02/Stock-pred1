fc2d457c8ee7e55bb9f0be47c0060f85bd8d1274
RoyR02 <104689216+RoyR02@users.noreply.github.com>
2024-08-10 20:05:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,9 @@ st.title('Stock Market Prediction')
 st.info('Stock Market Prediction using NN')
 
 with st.expander('Data'):
-  st.write('**Raw Data**)
-  df = pd
-df = pd.read_csv
+  st.write('**Raw Data**')
+  df  = pd.read_csv('https://github.com/RoyR02/Stock-pred1/blob/master/AAPL.csv')
+  st.write('**Date**')
+  x = df.drop('date', axis=1)
+
+
